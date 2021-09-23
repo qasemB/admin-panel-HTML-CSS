44f06aaece1baa9e2b36ebb6a3e55c851bd81fd2
qasemB <mahdicmptr@gmail.com>
2021-09-24 00:37:08 +0330

    after ok add product category page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
-var exampleEl = document.getElementById('example')
-var tooltip = new bootstrap.Tooltip(exampleEl, {})
+window.addEventListener('DOMContentLoaded', (event) => {
+
+
+    document.getElementById('handle_toggle_sidemenu').addEventListener('change' , function(){
+        if (this.checked) {
+            document.querySelector('.mini_sidebar').classList.add('expanded')
+            document.getElementById('content_section').classList.add('with_sidebar')
+        }else{
+            document.querySelector('.mini_sidebar').classList.remove('expanded')
+            document.getElementById('content_section').classList.remove('with_sidebar')
+        }
+    })
+
+});
