1331f7e802faf7649f664dfb013fa8b1d6e9dda6
qasemB <mahdicmptr@gmail.com>
2021-11-03 07:39:30 +0330

    after ok cards in dashboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
             document.getElementById('content_section').classList.remove('with_sidebar')
         }
     })
+
+    let sidebarItms = document.querySelectorAll('.sidebar_menu_item');
+    for (const item of sidebarItms) {
+        item.addEventListener('click' , ()=>{
+            for (const i of sidebarItms) {
+                i.classList.remove('active')
+            }
+            item.classList.add('active')
+            let sectionId = item.getAttribute('data-section-id');
+            let allSections = document.querySelectorAll('.main_section');
+            for (const section of allSections) {
+                section.classList.add('d-none')
+            }
+            document.getElementById(`${sectionId}`).classList.remove('d-none')
+        })
+    }
     
 });
 
